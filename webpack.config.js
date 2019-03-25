9482dc6bbe34902bd5022e4854c6dc725712348d
이환섭 <raphael@buzzni.com>
2019-03-25 14:57:59 +0900

    development mode, sourcemap code added

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
 
 module.exports = {
+    mode: 'development',
     entry: {
         app: './src/index.js'
     },
@@ -11,6 +12,7 @@ module.exports = {
         filename: '[name].[contenthash].js',
         path: path.resolve(__dirname, 'dist')
     },
+    devtool: 'inline-source-map',
     optimization: { // 목적: 업데이트 시 같은 파일명으로 배포 시 브라우저 캐싱으로 인해 업데이트가 이뤄지지 않는다.
         runtimeChunk: 'single', // 런타임은 웹팩 환경으로 파일들에서 import나 require 등이 문법에 상관 없이 __webpack_require__ 로 변환되는 등의 작업을 한다.
         splitChunks: { // node_modules에서 import 한 파일들은 수정될 일이 거의 없으므로 따로 chunk하여 캐싱되게 한다.
@@ -23,7 +25,7 @@ module.exports = {
             }
         }
     }, // 이렇게 구성되면 배포 시 src에 있는 파일 변동에만 hash를 적용하여 캐싱을 방지한다.
-    module: {
+    module: { // 리소스 파일 내에서 아래 확장자를 import하여 사용하기 위한 모듈이다.
         rules: [
             {
                 test: /\.css$/,
@@ -51,6 +53,7 @@ module.exports = {
         new HtmlWebpackPlugin({
             title: 'Caching'
         }),
-        new webpack.HashedModuleIdsPlugin()
+        // new NamedModulesPlugin() // 가독성 때문에 development 환경에서 유용하다.
+        new webpack.HashedModuleIdsPlugin() // hasedModuleIdsPlugin은 production 환경에서 추천된다.
     ]
 }
